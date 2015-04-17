9206d4e9680da02b674d02274537718552b128f7
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-04-17 10:34:24 +0200

    refactored to use private functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,101 +1,103 @@
-var app = {
-  categories: [],
-  selectedCategories: [],
-  selectedTemplate: null,
-  listTemplate: null,
-  notificationTemplate: null,
+var app = (function () {
+  var categories = [],
+    selectedCategories = [],
+    selectedTemplate = null,
+    listTemplate = null,
+    notificationTemplate = null;
 
-  renderCategories: function () {
-    document.getElementById("list").innerHTML = this.listTemplate({categories: this.categories});
-  },
+  function renderCategories() {
+    document.getElementById("list").innerHTML = listTemplate({categories: categories});
+  }
 
-  fadeOutMessage: function (message) {
+  function fadeOutMessage(message) {
     var notifyElement = document.getElementById('notify');
-    notifyElement.innerHTML = app.notificationTemplate({message: message});
+    notifyElement.innerHTML = notificationTemplate({message: message});
     window.setTimeout(function () {
       notifyElement.innerHTML = '';
     }, 3000);
-  },
+  }
 
-  initialize: function () {
-    this.listTemplate = Handlebars.compile(document.getElementById('category-template').innerText);
-    this.selectedTemplate = Handlebars.compile(document.getElementById('selected-template').innerText);
-    this.notificationTemplate = Handlebars.compile(document.getElementById('notification-template').innerText);
-    $fh.cloud({
-      path: '/api/',
-      method: 'GET'
-    }, function (res) {
-      app.categories = res.data;
-      app.renderCategories();
-    });
-  },
+  function renderTo() {
+    document.getElementById('selected').innerHTML = selectedTemplate({selected: selectedCategories});
+    document.getElementById('submit').disabled = selectedCategories.length === 0;
+  }
 
-  renderTo: function () {
-    document.getElementById('selected').innerHTML = this.selectedTemplate({selected: this.selectedCategories});
-    document.getElementById('submit').disabled = this.selectedCategories.length === 0;
-  },
-
-  selectionChanged: function (option) {
-    if (option.checked) {
-      this.selectedCategories.push(option.id);
-    } else {
-      this.selectedCategories.splice(this.selectedCategories.indexOf(option.id), 1);
-    }
-    this.renderTo();
-  },
-
-  addCategory: function (input) {
-    if (event.keyCode === 13) {
+  return {
+    initialize: function () {
+      listTemplate = Handlebars.compile(document.getElementById('category-template').innerText);
+      selectedTemplate = Handlebars.compile(document.getElementById('selected-template').innerText);
+      notificationTemplate = Handlebars.compile(document.getElementById('notification-template').innerText);
       $fh.cloud({
         path: '/api/',
+        method: 'GET'
+      }, function (res) {
+        categories = res.data;
+        renderCategories();
+      });
+    },
+
+    selectionChanged: function (option) {
+      if (option.checked) {
+        selectedCategories.push(option.id);
+      } else {
+        selectedCategories.splice(selectedCategories.indexOf(option.id), 1);
+      }
+      renderTo();
+    },
+
+    addCategory: function (input) {
+      if (event.keyCode === 13) {
+        $fh.cloud({
+          path: '/api/',
+          data: {
+            name: input.value
+          }
+        }, function () {
+          categories.push(input.value);
+          renderCategories();
+          input.value = '';
+        });
+      }
+    },
+
+    selectAll: function (source) {
+      selectedCategories = [];
+      var checkboxes = document.getElementsByName('category');
+      for (var i = 0, n = checkboxes.length; i < n; i++) {
+        checkboxes[i].checked = source.checked;
+        if (source.checked) {
+          selectedCategories.push(checkboxes[i].id);
+        }
+      }
+      renderTo();
+    },
+
+    deleteCategory: function (name) {
+      $fh.cloud({
+        path: '/api/' + name,
+        method: 'DELETE'
+      }, function () {
+        categories.splice(categories.indexOf(name), 1);
+        var index = selectedCategories.indexOf(name);
+        if (index !== -1) {
+          selectedCategories.splice(index, 1);
+        }
+
+        renderCategories();
+      });
+    },
+
+    send: function () {
+      var message = document.getElementById('pushmessage').value;
+      $fh.cloud({
+        path: '/api/send',
         data: {
-          name: input.value
+          alert: message,
+          categories: selectedCategories
         }
       }, function () {
-        app.categories.push(input.value);
-        app.renderCategories();
-        input.value = '';
+        fadeOutMessage('Message sent!');
       });
     }
-  },
-
-  selectAll: function (source) {
-    this.selectedCategories = [];
-    var checkboxes = document.getElementsByName('category');
-    for (var i = 0, n = checkboxes.length; i < n; i++) {
-      checkboxes[i].checked = source.checked;
-      if (source.checked) {
-        this.selectedCategories.push(checkboxes[i].id);
-      }
-    }
-    this.renderTo();
-  },
-
-  deleteCategory: function (name) {
-    $fh.cloud({
-      path: '/api/' + name,
-      method: 'DELETE'
-    }, function () {
-      app.categories.splice(app.categories.indexOf(name), 1);
-      var index = this.selectedCategories.indexOf(name);
-      if (index !== -1) {
-        app.selectedCategories.splice(index, 1);
-      }
-
-      app.renderCategories();
-    });
-  },
-
-  send: function () {
-    var message = document.getElementById('pushmessage').value;
-    $fh.cloud({
-      path: '/api/send',
-      data: {
-        alert: message,
-        categories: this.selectedCategories
-      }
-    }, function () {
-      app.fadeOutMessage('Message sent!');
-    });
   }
-};
+}());
